43891142af6ef4fbecfc43e63623f97e6603c5a6
Leehyeok <dlgur12008@naver.com>
2024-10-13 18:05:21 +0900

    코드 수정 및 오류 처리 개선

--- frontend/src/pages/cottonCandyShop/CottonCandyShop.vue
+++ frontend/src/pages/cottonCandyShop/CottonCandyShop.vue
@@ -1,179 +1,188 @@
 <template>
-    <div class="cotton-candy-shop-page">
-      <div class="header">
-        <h1 class="page-title">모구리 상점</h1>
-        <!-- <div class="mascot-message">
-          <div class="speech-bubble">
-            <p>소비 패턴에 맞는 퀘스트를 원한다면 나를 클릭해구리!</p>
-          </div>
-          <img
-            src="@/assets/img/Moguri.png"
-            alt="Mascot"
-            class="mascot-image"
-            @click="showRecommendationModal"
-          />
-        </div> -->
-      </div>
-      <div class="character-list-container">
-        <!-- 코튼캔디 현황 -->
-        <div class="my-cotton-candy-flex d-flex flex-row-reverse">
-          <div class="my-cotton-candy-container p-1 m-2">
-            <div class="icon-circle">
-              <i class="fa-solid fa-cloud" style="color: #ffe5f2"></i>
-            </div>
-            <span class="my-cotton-candy" style="height: 1.5rem">70,000</span>
-          </div>
-        </div>
-        <ul class="character-cards">
-          <li v-for="(character, index) in characters" :key="character.MOGURI_ID">
-            <CharacterCard
-              :character="character"
-              :badgeColor="getBadgeColor(index)"
-              @purchase="openPurchaseModal"
-            />
-          </li>
-        </ul>
-      </div>
-      <PurchaseModal
-        v-if="isPurchaseModalVisible && selectedCharacter"
-        :isVisible="isPurchaseModalVisible"
-        :character="selectedCharacter"
-        @confirm="purchaseCharacter"
-        @cancel="closePurchaseModal"
-      />
-      <ConfirmationModal
-        v-if="isConfirmationModalVisible"
-        :isVisible="isConfirmationModalVisible"
-        :message="confirmationMessage"
-        @close="closeConfirmationModal"
-      />
+  <div class="cotton-candy-shop-page">
+    <div class="header">
+      <h1 class="page-title">모구리 상점</h1>
     </div>
-  </template>
-  
-  <script>
-  // 수정된 코드 (경로 대소문자 일치)
-  import CharacterCard from '@/components/Cottoncandyshop/CharacterCard.vue';
-  import PurchaseModal from '@/components/Cottoncandyshop/PurchaseModal.vue';
-  import ConfirmationModal from '@/components/Cottoncandyshop/ConfirmationModal.vue';
-  import moguriSampleImage from '@/assets/img/moguristore_sample_img.png';
-  
-  export default {
-    components: {
-      CharacterCard,
-      PurchaseModal,
-      ConfirmationModal,
+    <div class="character-list-container">
+      <!-- 코튼캔디 현황 -->
+      <div class="my-cotton-candy-flex d-flex flex-row-reverse">
+        <div class="my-cotton-candy-container p-1 m-2">
+          <div class="icon-circle">
+            <i class="fa-solid fa-cloud" style="color: #ffe5f2"></i>
+          </div>
+          <span class="my-cotton-candy" style="height: 1.5rem">{{ currentCottonCandy }}</span>
+        </div>
+      </div>
+      <ul class="character-cards">
+        <li v-for="(character, index) in characters" :key="character.MOGURI_ID">
+          <CharacterCard
+            :character="character"
+            :badgeColor="getBadgeColor(index)"
+            @purchase="openPurchaseModal"
+          />
+        </li>
+      </ul>
+    </div>
+    <PurchaseModal
+      v-if="isPurchaseModalVisible && selectedCharacter"
+      :isVisible="isPurchaseModalVisible"
+      :character="selectedCharacter"
+      @confirm="purchaseCharacter"
+      @cancel="closePurchaseModal"
+    />
+    <ConfirmationModal
+      v-if="isConfirmationModalVisible"
+      :isVisible="isConfirmationModalVisible"
+      :message="confirmationMessage"
+      @close="closeConfirmationModal"
+    />
+  </div>
+</template>
+
+<script>
+// 수정된 코드 (경로 대소문자 일치)
+import { useAuthStore } from '@/stores/auth'; 
+import CharacterCard from '@/components/Cottoncandyshop/CharacterCard.vue';
+import PurchaseModal from '@/components/Cottoncandyshop/PurchaseModal.vue';
+import ConfirmationModal from '@/components/Cottoncandyshop/ConfirmationModal.vue';
+import moguriSampleImage from '@/assets/img/moguristore_sample_img.png';
+
+export default {
+  components: {
+    CharacterCard,
+    PurchaseModal,
+    ConfirmationModal,
+  },
+  data() {
+    return {
+      characters: [
+        {
+          MOGURI_ID: 1,
+          MOGURI_NAME: '어쩌구 모구리',
+          MOGURI_PRICE: 100,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 2,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 200,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 3,
+          MOGURI_NAME: '어쩌구 모구리',
+          MOGURI_PRICE: 300,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 4,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 400,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 5,
+          MOGURI_NAME: '어쩌구 모구리',
+          MOGURI_PRICE: 500,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 6,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 100,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 7,
+          MOGURI_NAME: '어쩌구 모구리',
+          MOGURI_PRICE: 200,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 8,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 300,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 9,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 400,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+        {
+          MOGURI_ID: 10,
+          MOGURI_NAME: '저쩌구 모구리',
+          MOGURI_PRICE: 500,
+          MOGURI_IMAGE_PATH: moguriSampleImage,
+        },
+      ],
+      badgeColors: ['#E78160', '#F7E788', '#75BF7D', '#B8DAF4'],
+      isPurchaseModalVisible: false,
+      selectedCharacter: {},
+      isConfirmationModalVisible: false,
+      confirmationMessage: '',
+      memberId: null, // memberId 초기화
+    };
+  },
+  computed: {
+    currentCottonCandy() {
+      const authStore = useAuthStore();
+      return authStore.cottonCandy; // 동적으로 authStore에서 cottonCandy 값 가져오기
+    }
+  },
+  created() {
+    // useAuthStore로부터 memberId 가져오기
+    const authStore = useAuthStore();
+    this.memberId = authStore.memberId;
+  },
+  methods: {
+    getBadgeColor(index) {
+      return this.badgeColors[index % this.badgeColors.length];
     },
-    data() {
-      return {
-        characters: [
-          {
-            MOGURI_ID: 1,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 100,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 2,
-            MOGURI_NAME: '저쩌구 모구리',
-            MOGURI_PRICE: 200,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 3,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 300,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 4,
-            MOGURI_NAME: '저쩌구 모구리',
-            MOGURI_PRICE: 400,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 5,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 500,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 1,
-            MOGURI_NAME: '저쩌구 모구리',
-            MOGURI_PRICE: 100,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 2,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 200,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 3,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 300,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 4,
-            MOGURI_NAME: '저쩌구 모구리',
-            MOGURI_PRICE: 400,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 5,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 500,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 4,
-            MOGURI_NAME: '저쩌구 모구리',
-            MOGURI_PRICE: 400,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-          {
-            MOGURI_ID: 5,
-            MOGURI_NAME: '어쩌구 모구리',
-            MOGURI_PRICE: 500,
-            MOGURI_IMAGE_PATH: moguriSampleImage,
-          },
-        ],
-        badgeColors: ['#E78160', '#F7E788', '#75BF7D', '#B8DAF4'],
-        isPurchaseModalVisible: false,
-        selectedCharacter: {},
-        isConfirmationModalVisible: false,
-        confirmationMessage: '',
-      };
+    openPurchaseModal(character) {
+      this.selectedCharacter = character;
+      this.isPurchaseModalVisible = true;
     },
-    methods: {
-      getBadgeColor(index) {
-        return this.badgeColors[index % this.badgeColors.length];
-      },
-      openPurchaseModal(character) {
-        this.selectedCharacter = character;
-        this.isPurchaseModalVisible = true;
-      },
-      closePurchaseModal() {
-        this.isPurchaseModalVisible = false;
-        this.selectedCharacter = {};
-      },
-      purchaseCharacter() {
-        // 여기에 실제 구매 로직을 구현합니다.
-        // 예: API 호출, 상태 업데이트 등
-        console.log(`${this.selectedCharacter.MOGURI_NAME} 구매 완료!`);
+    closePurchaseModal() {
+      this.isPurchaseModalVisible = false;
+      this.selectedCharacter = {};
+    },
+    async purchaseCharacter() {
+      const authStore = useAuthStore();
+      const price = this.selectedCharacter.MOGURI_PRICE;
+
+      if (this.currentCottonCandy < price) {
+        this.confirmationMessage = '코튼 캔디가 부족합니다.';
+        this.isConfirmationModalVisible = true;
+        return;
+      }
+
+      try {
+        // 모구리 구매 API 호출
+        await fetch(`/api/moguri/${this.selectedCharacter.MOGURI_ID}/${this.memberId}`, {
+          method: 'POST',
+        });
+
+        // 코튼 캔디 업데이트 (음수로 전달하여 줄어들게)
+        await authStore.updateCottonCandy(-price);
+
         this.confirmationMessage = `${this.selectedCharacter.MOGURI_NAME} 구매가 완료되었습니다!`;
         this.closePurchaseModal();
         this.isConfirmationModalVisible = true;
-      },
-      closeConfirmationModal() {
-        this.isConfirmationModalVisible = false;
-        this.confirmationMessage = '';
-      },
+      } catch (error) {
+        console.error('Error purchasing moguri:', error);
+      }
     },
-  };
-  </script>
-  
+    closeConfirmationModal() {
+      this.isConfirmationModalVisible = false;
+      this.confirmationMessage = '';
+    },
+  },
+
+};
+</script>
+
   <style scoped>
   div {
     /* border: 1px solid blue; */
